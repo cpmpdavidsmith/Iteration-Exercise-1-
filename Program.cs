4759535aabb57742b17aaef8a300a61e24062adc
David Smith <cpmpdavidsmith@icloud.com>
2023-02-08 09:30:54 -0500

    Exc1 15 times

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//------1
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+//12
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{i}");
+}
